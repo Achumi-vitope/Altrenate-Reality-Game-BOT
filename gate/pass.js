fcf45f62007c1f13e82b24bf67772d9c016a6771
Achumi-vitope <75613092+Achumi-vitope@users.noreply.github.com>
2021-11-27 13:21:22 +0530

    Update pass.js

--- gate/pass.js
+++ gate/pass.js
@@ -1,7 +1,7 @@
 
 function check()
 {
-	var key = "teBzZQJ#$v8SuPDNq!k79XkFm3VfZPL$2KhAYu2Z^v%FhvQ@45jv7X=Zjr!9-7_RVzP4??RVVR3+n@U%htq%#8Mb5wWk^Thw9xW5SWvyB7zafrVUM3nxV9@NL3xwVH^W";
+	var key = "ihaveadoubt";
 	var text = document.getElementById('passText').value;
 
 	if(text == key)
